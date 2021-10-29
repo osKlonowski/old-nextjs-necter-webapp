ec83ac3150d998d6607341cfa3b82486ad7edfc7
osKlonowski <osklonowski@gmail.com>
2021-10-29 16:30:24 +0200

    repushing to test github actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({ allPostsData }: {allPostsData: any}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[This is a demo website]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
